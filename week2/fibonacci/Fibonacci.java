a52119fb165fae054a15afd986c385393a060919
jiniC <jinic323@gmail.com>
2017-10-22 17:14:12 +0900

    week2_1 - submit

--- week2/fibonacci/Fibonacci.java
+++ week2/fibonacci/Fibonacci.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class Fibonacci {
   private static long calc_fib(int n) {
-    if (n <= 1)
-      return n;
-
-    return calc_fib(n - 1) + calc_fib(n - 2);
+    int[] fibo = new int[46];
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for (int i=2; i<=45; i++) {
+      fibo[i] = fibo[i-1]+fibo[i-2];
+    }
+    return fibo[n];
   }
 
   public static void main(String args[]) {
