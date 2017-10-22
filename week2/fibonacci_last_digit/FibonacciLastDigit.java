5f8963fa0098937fe884bf018fa4946493988159
jiniC <jinic323@gmail.com>
2017-10-22 22:11:09 +0900

    week2_2 - submit

--- week2/fibonacci_last_digit/FibonacciLastDigit.java
+++ week2/fibonacci_last_digit/FibonacciLastDigit.java
@@ -11,15 +11,16 @@ public class FibonacciLastDigit {
         for (int i = 0; i < n - 1; ++i) {
             int tmp_previous = previous;
             previous = current;
-            current = tmp_previous + current;
+            current = (tmp_previous + current)%10;
         }
 
-        return current % 10;
+        return current;
     }
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        scanner.close();
         int c = getFibonacciLastDigitNaive(n);
         System.out.println(c);
     }
