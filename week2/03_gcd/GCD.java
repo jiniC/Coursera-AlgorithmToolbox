ada50601ca26bfb33eb06989949f993b302fd94c
jiniC <jinic323@gmail.com>
2017-10-22 23:08:37 +0900

    week2_3 - submit

--- week2/03_gcd/GCD.java
+++ week2/03_gcd/GCD.java
@@ -1,24 +1,22 @@
 import java.util.*;
 
 public class GCD {
-  private static int gcd_naive(int a, int b) {
-    int current_gcd = 1;
-    for(int d = 2; d <= a && d <= b; ++d) {
-      if (a % d == 0 && b % d == 0) {
-        if (d > current_gcd) {
-          current_gcd = d;
-        }
-      }
+  private static int gcd(int a, int b) {
+    int gcd = 1;
+    int temp = 0;
+    while(b!=0) {
+      temp = a;
+      a = b;
+      b = temp%b;
     }
-
-    return current_gcd;
+    gcd = a;
+    return gcd;
   }
 
   public static void main(String args[]) {
     Scanner scanner = new Scanner(System.in);
     int a = scanner.nextInt();
     int b = scanner.nextInt();
-
-    System.out.println(gcd_naive(a, b));
+    System.out.println(gcd(a, b));
   }
 }
