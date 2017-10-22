353f4fec37a5f441a46d0b810d2bec61ee60f371
jiniC <jinic323@gmail.com>
2017-10-22 23:38:46 +0900

    week2_4 - submit

--- week2/04_lcm/LCM.java
+++ week2/04_lcm/LCM.java
@@ -1,19 +1,27 @@
 import java.util.*;
 
 public class LCM {
-  private static long lcm_naive(int a, int b) {
-    for (long l = 1; l <= (long) a * b; ++l)
-      if (l % a == 0 && l % b == 0)
-        return l;
+  private static int gcd(int a, int b) {
+    int gcd = 1;
+    int temp = 0;
+    while(b!=0) {
+      temp = a;
+      a = b;
+      b = temp%b;
+    }
+    gcd = a;
+    return gcd;
+  }
 
-    return (long) a * b;
+  private static long lcm(int a, int b) {
+    return (long) a*b / gcd(a,b);
   }
 
   public static void main(String args[]) {
     Scanner scanner = new Scanner(System.in);
     int a = scanner.nextInt();
     int b = scanner.nextInt();
-
-    System.out.println(lcm_naive(a, b));
+    scanner.close();
+    System.out.println(lcm(a, b));
   }
 }
