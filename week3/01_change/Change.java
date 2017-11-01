97e0efa4ad80b7f61616d388c581eee8e0599d62
jiniC <jinic323@gmail.com>
2017-11-01 23:32:13 +0900

    week3_1 - submit

--- week3/01_change/Change.java
+++ week3/01_change/Change.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class Change {
     private static int getChange(int m) {
         //write your code here
-        return m;
+        // 10, 5, 1
+        int result = 0;
+        int m_ten = m/10;
+        int m_five = (m-(m_ten*10))/5;
+        int m_one = m-(m_ten*10)-(m_five*5);
+        result = m_ten + m_five + m_one;
+        return result;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int m = scanner.nextInt();
         System.out.println(getChange(m));
-
     }
 }
 
